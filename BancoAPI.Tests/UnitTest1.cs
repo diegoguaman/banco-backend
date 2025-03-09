ea3e916666bc29b95837347aa939f328826746b5
Diego Guaman <diegoguamanmedina@gmail.com>
2025-03-09 19:51:13 +0100

    Add Unit Test with xUnit

---
+++ BancoAPI.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+public class UnitTest1
+{
+    [Fact] // Indica que esta es una prueba unitaria
+    public void TestSuma()
+    {
+        int resultado = 2 + 2;
+        Assert.Equal(4, resultado); // Verifica que 2 + 2 realmente sea 4
+    }
+}
+
